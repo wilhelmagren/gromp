cc7aef5f626162e35c955ebaadd127ad992cdfd8
willeagren <wilhelmagren98@gmail.com>
2023-02-05 20:12:55 +0100

    New changes to League(Hook) following rewrite

--- gromp/hook/league.py
+++ gromp/hook/league.py
@@ -1,87 +1,80 @@
-#
-# MIT License
-#
-# Copyright (c) 2023 Wilhelm Ågren
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-# File created: 2023-01-23
-# Last updated: 2023-02-02
-#
+"""
+MIT License
 
-from gromp.hook import BaseHook
-from gromp.api.league import *
-from gromp.utils import LeaguePLATFORMS, LeagueREGIONS
+Copyright (c) 2023 Wilhelm Ågren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2023-01-23
+Last updated: 2023-02-05
+"""
+
+from gromp.hook import Hook
+from gromp.endpoint import NamedEndpoint
+from gromp.endpoint.league import (
+    Accountv1,
+    Leaguev4,
+    LolStatusv4,
+    Matchv5,
+    Spectatorv4,
+    Summonerv4,
+    TournamentStubv4,
+)
+from gromp.utils import LeaguePlatforms, LeagueRegions
 
 __all__ = (
-    'LeagueHook',
+    'League',
 )
 
-class LeagueHook(BaseHook):
-    def __init__(self, token, platform=LeaguePLATFORMS.euw1, region=LeagueREGIONS.europe):
-        super().__init__(token, 'league', platform, region)
+class League(Hook):
+    def __init__(
+        self,
+        token: str,
+        **kwargs
+    ) -> None:
+        super().__init__(token, self.__class__.__name__, **kwargs)
 
-    def summoner_by_name(self, summonerName, platform=None, region=None):
-        if platform is None:
-            platform = self._platform
-        
-        if region is None:
-            region = self._region
+    @property
+    def account(self) -> Accountv1:
+        return self._setup_named_endpoint(Accountv1)
 
-        api = SummonerAPIv4(platform=platform, region=region)
-        response = api.summonerName(self._token, summonerName)
+    @property
+    def league(self) -> Leaguev4:
+        return self._setup_named_endpoint(Leaguev4)
 
-        return response.json()
+    @property
+    def lol_status(self) -> LolStatusv4:
+        return self._setup_named_endpoint(LolStatusv4)
 
-    
-    def summoner_by_puuid(self, puuid, platform=None, region=None):
-        if platform is None:
-            platform = self._platform
+    @property
+    def match(self) -> Matchv5:
+        return self._setup_named_endpoint(Matchv5)
 
-        if region is None:
-            region = self._region
-        
-        api = SummonerAPIv4(platform=platform, region=region)
-        response = api.encryptedPUUID(self._token, puuid)
-        
-        return response.json()
-    
-    def matches_by_puuid(self, puuid, platform=None, region=None, **kwargs):
-        if platform is None:
-            platform = self._platform
-        
-        if region is None:
-            region = self._region
-        
-        api = MatchAPIv5(platform=platform, region=region)
-        response = api.puuid(self._token, puuid, **kwargs)
+    @property
+    def spectator(self) -> Spectatorv4:
+        return self._setup_named_endpoint(Spectatorv4)
 
-        return response.json()
-    
-    def match_by_id(self, matchId, platform=None, region=None):
-        if platform is None:
-            platform = self._platform
-        
-        if region is None:
-            region = self._region
-        
-        api = MatchAPIv5(platform=platform, region=region)
-        response = api.matchId(self._token, matchId)
+    @property
+    def summoner(self) -> Summonerv4:
+        return self._setup_named_endpoint(Summonerv4)
 
-        return response.json()
+    @property
+    def tournament_stub(self) -> TournamentStubv4:
+        return self._setup_named_endpoint(TournamentStubv4)
+
