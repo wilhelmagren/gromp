d48b9328f0c8856dec419b62947d4f5c102cf714
willeagren <wilhelmagren98@gmail.com>
2023-02-12 11:12:46 +0100

    Implement League hook tests (#8)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,17 @@
-# Leave this file empty. Only exists such that -m unittest finds the tests files.
+import inspect
+import pkgutil
+import unittest
+
+def load_tests(loader, suite, pattern):
+    """
+    Import all local testing submodules here, so that ``gromp``
+    is accessible for even the deeper tests.
+    https://stackoverflow.com/questions/29713541/recursive-unittest-discover
+    """
+    for imp, modname, _ in pkgutil.walk_packages(__path__):
+        mod = imp.find_module(modname).load_module(modname)
+        for test in loader.loadTestsFromModule(mod):
+            suite.addTests(test)
+
+    return suite
+
