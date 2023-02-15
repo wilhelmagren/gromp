fa3a0b132dc35d2bfcb42e6d75f01f6fcedb21c7
willeagren <wilhelmagren98@gmail.com>
2023-02-15 11:42:07 +0100

    Add input checks, static typing

--- gromp/hook/base.py
+++ gromp/hook/base.py
@@ -5,8 +5,8 @@ Copyright (c) 2023 Wilhelm Ågren
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+in the Software without reStringiction, including without limitation the rights
+to use, copy, modify, merge, publish, diStringibute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
@@ -22,10 +22,25 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
 File created: 2023-01-23
-Last updated: 2023-02-14
+Last updated: 2023-02-15
 """
 
+from __future__ import annotations
+
 import rsa
+import builtins
+String = builtins.str
+Integer = builtins.int
+
+from typing import (
+    List,
+    Dict,
+    Optional,
+    Union,
+    Any,
+    NoReturn,
+)
+
 from gromp.utils import (
     is_platform,
     is_region,
@@ -42,28 +57,39 @@ __all__ = (
 
 class Hook(object):
     def __init__(
-        self,
-        token: str = None,
-        game: str = None,
-        platform: str = None,
-        region: str = None,
-        handlers: list = None,
-        keylen: int = 512,
-        timeout: int = 5,
-        **kwargs
-    ) -> None:
+        self: Hook,
+        token: Optional[String] = None,
+        game: Optional[String] = None,
+        platform: Optional[String] = None,
+        region: Optional[String] = None,
+        handlers: Optional[List] = None,
+        keylen: Optional[Integer] = 512,
+        timeout: Optional[Integer] = 5,
+        **kwargs: Dict,
+    ) -> NoReturn:
+
+        if region is None:
+            assert game in ('Valorant'), \
+                f'No region was specified, only games `Valorant` support only using ' \
+                f'platform string for API requests. Here are the regions for your ' \
+                f'specified game: {vars(getattr(REGIONS, game)).values()}.'
+
+        if game not in ('Valorant'):
+            assert is_region(region, game.lower()), \
+                f'User provided region is not valid, {region=}.\n' \
+                f'Valid regions are: {vars(getattr(REGIONS, game)).values()}.'
 
         assert is_platform(platform, game.lower()), \
             f'User provided platform is not valid, {platform=}.\n' \
             f'Valid platforms are: {vars(getattr(PLATFORMS, game)).values()}.'
 
-        assert is_region(region, game.lower()), \
-            f'User provided region is not valid, {region=}.\n' \
-            f'Valid regions are: {vars(getattr(REGIONS, game)).values()}.'
-
         assert timeout > 0, \
             f'You have provided a timeout value <= 0 which is not valid.\n' \
             f'This sets the allowed time to wait for response, defaults to 5 seconds.'
+
+        assert keylen > 0, \
+            f'You have provided a key length value <= 0 which is not valid. ' \
+            f'Please specify a larger value for the RSA algorithm to work properly.'
 
         public_key, private_key = rsa.newkeys(keylen)
         encrypted_token = rsa.encrypt(
@@ -92,30 +118,30 @@ class Hook(object):
         self._handlers = handlers
 
     @property
-    def token(self) -> str:
+    def token(self) -> String:
         return rsa.decrypt(
             self._config['token'],
             self._config['keys']['private'],
         ).decode()
 
     @property
-    def game(self) -> str:
+    def game(self) -> String:
         return self._config['game']
 
     @property
-    def platform(self) -> str:
+    def platform(self) -> String:
         return self._config['platform']
     
     @property
-    def region(self) -> str:
+    def region(self) -> String:
         return self._config['region']
 
     @property
-    def timeout(self) -> int:
+    def timeout(self) -> Integer:
         return self._config['timeout']
 
     @property
-    def keys(self) -> dict:
+    def keys(self) -> Dict:
         return self._config['keys']
 
     @property
@@ -123,11 +149,11 @@ class Hook(object):
         return self._config['keys']['public_key']
 
     @property
-    def config(self) -> dict:
+    def config(self) -> Dict:
         return self._config
 
     @property
-    def handlers(self) -> list:
+    def handlers(self) -> List:
         return self._handlers
 
     def _setup_named_endpoint(
