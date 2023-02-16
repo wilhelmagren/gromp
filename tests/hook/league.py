d9aa0187803a1d46a0534cd37bbd72b8d115e2cc
willeagren <wilhelmagren98@gmail.com>
2023-02-16 11:25:49 +0100

    Testing all endpoint GET requests (#28)

--- tests/hook/league.py
+++ tests/hook/league.py
@@ -166,43 +166,273 @@ class LeagueHookTest(unittest.TestCase):
         endpoint = self.hook.league_exp
         self.assertEqual(endpoint.__class__, LeagueExpv4)
 
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testLeagueExpv4Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.league_exp,
+            [
+                'by_queue_tier_division',
+                'by_queue_tier_division',
+                'by_queue_tier_division',
+                'by_queue_tier_division',
+            ],
+            [
+                ['2', '1', '3'],
+                ['superSigma', 'based', 'bateman'],
+                ['4', '2', '5'],
+                ['xdlol', 'tier', '4'],
+            ],
+            [
+                200,
+                404,
+                200,
+                404,
+            ],
+        )
+
     def testLeaguev4(self):
         logger.debug('Leaguev4 property...')
         endpoint = self.hook.league
         self.assertEqual(endpoint.__class__, Leaguev4)
+
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testLeaguev4Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.league,
+            [
+                'challenger_by_queue', 'challenger_by_queue',
+                'by_summoner_id', 'by_summoner_id',
+                'by_queue_tier_division', 'by_queue_tier_division',
+                'grandmaster_by_queue', 'grandmaster_by_queue',
+                'by_id', 'by_id',
+                'master_by_queue', 'master_by_queue',
+            ],
+            [
+                ['2'], ['xdlolhaha'],
+                ['1010101'], ['this is my super sigma id'],
+                ['2', '1', '3'], ['based', 'bateman', 'gamer'],
+                ['1'], ['big man queue'],
+                ['1928491841'], ['secret ID dont look'],
+                ['1'], ['coffee queue'],
+            ],
+            [
+                200, 404,
+                200, 404,
+                200, 404,
+                200, 404,
+                200, 404,
+            ],
+        )
 
     def testLolChallengesv1(self):
         logger.debug('LolChallengesv1 property...')
         endpoint = self.hook.challenges
         self.assertEqual(endpoint.__class__, LolChallengesv1)
 
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testLolChallengesv1Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.challenges,
+            [
+                'config',
+                'percentiles',
+                'challenge_config', 'challenge_config',
+                'top_players_for_challenge', 'top_players_for_challenge',
+                'top_players_for_challenge',
+                'challenge_percentile', 'challenge_percentile',
+                'player_data_by_puuid', 'player_data_by_puuid',
+            ],
+            [
+                [],
+                [],
+                ['400'], ['big banana challenge'],
+                ['123', '2'], ['41', '2', '20'],
+                ['xd lol ahha', 'avocado'],
+                ['85'], ['this is a challenge id i promise'],
+                ['91849189481948149'], ['this is my secret puuid'],
+            ],
+            [
+                200,
+                200,
+                200, 404,
+                200, 200,
+                404,
+                200, 404,
+                200, 404,
+            ],
+        )
+
     def testLolStatusv4(self):
         logger.debug('LolStatusv4 property...')
         endpoint = self.hook.status
         self.assertEqual(endpoint.__class__, LolStatusv4)
+
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testLolStatusv4Respones(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.status,
+            ['get'],
+            [[]],
+            [200],
+        )
 
     def testMatchv5(self):
         logger.debug('Matchv5 property...')
         endpoint = self.hook.match
         self.assertEqual(endpoint.__class__, Matchv5)
 
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testMatchv5Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.match,
+            [
+                'matchlist_by_puuid', 'matchlist_by_puuid',
+                'matchlist_by_puuid', 'matchlist_by_puuid',
+                'by_id', 'by_id',
+                'timeline_by_id', 'timeline_by_id',
+            ],
+            [
+                ['41412081729837'], ['81723871283', '2', '1', '5', '10'],
+                ['cookie man'], ['elden ring goty'],
+                ['948194'], ['this is a match id i promise'],
+                ['9481927'], ['this is not a match id'],
+            ],
+            [
+                200, 200,
+                404, 404,
+                200, 404,
+                200, 404,
+            ],
+        )
+
     def testSpectatorv4(self):
         logger.debug('Spectatorv4 property...')
         endpoint = self.hook.spectator
         self.assertEqual(endpoint.__class__, Spectatorv4)
+
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testSpectatorv4Respones(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.spectator,
+            [
+                'by_summoner_id', 'by_summoner_id',
+                'featured_games',
+            ],
+            [
+                ['1481274819'], ['baby yoda'],
+                [],
+            ],
+            [
+                200, 404,
+                200,
+            ],
+        )
 
     def testSummonerv4(self):
         logger.debug('Summonerv4 property...')
         endpoint = self.hook.summoner
         self.assertEqual(endpoint.__class__, Summonerv4)
 
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testSummonerv4Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.summoner,
+            [
+                'by_account', 'by_account',
+                'by_name', 'by_name',
+                'by_puuid', 'by_puuid',
+                'by_id', 'by_id',
+            ],
+            [
+                ['f183798f19jk98'], ['slime rancher'],
+                ['1 900 976 JUICE'], ['pure4silver'],
+                ['981293816471'], ['water man'],
+                ['faa89ajhg8ga546124'], ['encrypted summoner id xd'],
+            ],
+            [
+                200, 404,
+                200, 404,
+                200, 404,
+                200, 404,
+            ],
+        )
+
     def testTournamentStubv4(self):
         logger.debug('TournamentStubv4 property...')
         endpoint = self.hook.tournament_stub
         self.assertEqual(endpoint.__class__, TournamentStubv4)
 
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testTournamentStubv4Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.tournament_stub,
+            [
+                'codes',
+                'events_by_code', 'events_by_code', 
+                'providers',
+                'tournaments',
+            ],
+            [
+                [],
+                ['87149812814'], ['super secret LEC code'],
+                [],
+                [],
+            ],
+            [
+                200,
+                200, 404,
+                200,
+                200,
+            ],
+        )
+
     def testTournamentv4(self):
         logger.debug('Tournamentv4 property...')
         endpoint = self.hook.tournament
         self.assertEqual(endpoint.__class__, Tournamentv4)
+
+    @patch('gromp.endpoint.NamedEndpoint._request_api')
+    def testTournamentv4Responses(self, mock_get):
+        _testEndpointRequests(
+            self,
+            mock_get,
+            self.hook.tournament,
+            [
+                'codes',
+                'dto_by_code', 'dto_by_code',
+                'events_by_code', 'events_by_code',
+                'providers',
+                'tournaments',
+            ],
+            [
+                [],
+                ['98afhufy87561287'], ['coderino'],
+                ['1927488varf878ga'], ['book book book'],
+                [],
+                [],
+            ],
+            [
+                200,
+                200, 404,
+                200, 404,
+                200,
+                200,
+            ],
+        )
 
