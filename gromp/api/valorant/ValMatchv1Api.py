c621ede43fccfef8c7940b06f87b12d8e143cb96
willeagren <wilhelmagren98@gmail.com>
2023-02-23 00:16:13 +0100

    Implement Valorant APIs (#33)

---
+++ gromp/api/valorant/ValMatchv1Api.py
@@ -0,0 +1,59 @@
+"""
+MIT License
+
+Copyright (c) 2023 Wilhelm Ågren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2023-02-23
+Last updated: 2023-02-23
+"""
+
+from __future__ import annotations
+
+import builtins
+String = builtins.str
+
+from typing import NoReturn
+from gromp.api.base import ValorantApi
+
+__all__ = (
+    'ValMatchv1Api',
+)
+
+class ValMatchv1Api(ValorantApi):
+
+    api = {
+
+        'by_id':
+        'matches/{match_id}',
+
+        'matchlist_by_puuid':
+        'matchlists/by-puuid/{puuid}',
+
+        'recent_by_queue':
+        'recent-matches/by-queue/{queue}',
+
+    }
+
+    def __init__(self, key: String) -> NoReturn:
+        super(ValMatchv1Api, self).__init__(
+            '{region}', f'match/v1/{self.api[key]}',
+        )
+
---
+++ gromp/api/valorant/ValRankedv1Api.py
@@ -0,0 +1,54 @@
+"""
+MIT License
+
+Copyright (c) 2023 Wilhelm Ågren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2023-02-23
+Last updated: 2023-02-23
+"""
+
+from __future__ import annotations
+
+import builtins
+String = builtins.str
+
+from typing import NoReturn
+
+from gromp.api.base import ValorantApi
+
+__all__ = (
+    'ValRankedv1Api',
+)
+
+class ValRankedv1Api(ValorantApi):
+
+    api = {
+
+        'by_act':
+        '{act_id}'
+
+    }
+
+    def __init__(self, key: String) -> NoReturn:
+        super(ValRankedv1Api, self).__init__(
+            '{region}', f'ranked/v1/leaderboards/by-act/{self.api[key]}',
+        )
+
