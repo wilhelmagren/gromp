ac74d0cd10cf1de93d55d6236604ba7ed0b5ac04
willeagren <wilhelmagren98@gmail.com>
2023-02-23 17:57:37 +0100

    Implement Accountv1 API (#33)

---
+++ gromp/api/accounts/Accountv1Api.py
@@ -0,0 +1,62 @@
+"""
+MIT License
+
+Copyright (c) 2023 Wilhelm Ågren
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+File created: 2023-02-23
+Last updated: 2023-02-23
+"""
+
+from __future__ import annotations
+
+import builtins
+String = builtins.str
+
+from typing import NoReturn
+
+from gromp.api.base import AccountsApi
+
+__all__ = (
+    'Accountv1Api',
+)
+
+class Accountv1Api(AccountsApi):
+
+    api = {
+
+        'by_puuid':
+        'accounts/by-puuid/{puuid}',
+
+        'by_riot_id':
+        'accounts/by-riot-id/{game_name}/{tag_line}',
+
+        'me':
+        'accounts/me',
+
+        'active_shard':
+        'active-shards/by-game/{game}/by-puuid/{puuid}',
+    }
+
+    def __init__(self, key: String) -> NoReturn:
+        super(Accountv1Api, self).__init__(
+            '{region}', f'account/v1/{self.api[key]}',
+        )
+
