55a470fafb575050fc72e5fa264f368cb234460d
willeagren <wilhelmagren98@gmail.com>
2023-02-27 22:23:27 +0100

    Bump version for setup 0.6.0 → 0.8.0

--- setup.py
+++ setup.py
@@ -6,13 +6,12 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='gromp',
-    version='0.6.0',
+    version='0.8.0',
     author='Wilhelm Ågren',
     author_email='wilhelmagren98@gmail.com',
     packages=find_packages(),
     url='https://github.com/willeagren/gromp',
     classifiers=[
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
